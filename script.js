e33c266381acd3fc384795ac6a133f8094e29965
ThomFri <36042923+ThomFri@users.noreply.github.com>
2020-10-24 15:43:53 +0200

    Update script.js
    LAMP -> lamp

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Definiere die onload-Funktion, die nach dem Laden des erhaltenen HTML ausgeführt werden soll.
+//Definiere die onload-Funktion, die nach dem Laden des erhaltenen HTML ausgefÃ¼hrt werden soll.
 function onload(){
 	//Greife auf das DOM zu, um Buttons in Variable abzulegen.
 	let increaseButtons = document.querySelectorAll(".increase");
@@ -6,36 +6,36 @@ function onload(){
 	
 	//Durchlauf alle vorhandenen 'increase'-Buttons.
 	for (let button of increaseButtons){
-		//Füge dem aktuellen Button einen 'click-Listener' hinzu, 
-		//eine Funktion die ausgeführt wird, wenn der Nutzer auf den Button clickt.
+		//FÃ¼ge dem aktuellen Button einen 'click-Listener' hinzu, 
+		//eine Funktion die ausgefÃ¼hrt wird, wenn der Nutzer auf den Button clickt.
 		button.addEventListener("click", function(){
 			//Speichere die Produkt ID und die aktuelle Anzahl.
 			let pid = button.getAttribute('pid');
 			let stock = document.querySelector('div[pid="'+pid+'"]');
 			
-			//Wandle die Anzahl in einen integer um und erhöhe diesen um 1.
+			//Wandle die Anzahl in einen integer um und erhÃ¶he diesen um 1.
 			let value = parseInt(stock.innerHTML)+1;
 			
 			//Erzeuge AJAX-Anfrage, um die Daten an data.php zu senden.
 			
 			//Speichere die URL.
-			let ajaxurl = '/dashboard_LAMP/data.php',
-			//Stelle die Daten zusammen, die übertragen werden sollen.
+			let ajaxurl = '/dashboard_lamp/data.php',
+			//Stelle die Daten zusammen, die Ã¼bertragen werden sollen.
 			data =  "pid= "+pid+"& stock="+value; 
 			
 			//Erstelle die HTML-Anfrage
 			var request = new XMLHttpRequest();
-			//Erstelle die Funktion, den HTTP-Response empfängt.
+			//Erstelle die Funktion, den HTTP-Response empfÃ¤ngt.
 			request.onreadystatechange = function () {
                 if (request.readyState == 4 && request.status == 200) {
                     console.log(request.responseText);
-					//Erhöhe die Anzahl auf dem Bildschirm um 1,
-					//nachdem die Server-Anfrage ausgeführt wurde.
+					//ErhÃ¶he die Anzahl auf dem Bildschirm um 1,
+					//nachdem die Server-Anfrage ausgefÃ¼hrt wurde.
 					stock.innerHTML = parseInt(stock.innerHTML)+1;
                 }
             };
 			
-			//Führe Anfrage aus.
+			//FÃ¼hre Anfrage aus.
 			request.open('POST', ajaxurl, true);
 			request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 			request.send(data);
@@ -61,12 +61,12 @@ function onload(){
 				if (request.readyState == 4 && request.status == 200) {
 					console.log(request.responseText);
 					//Verringere die Anzahl auf dem Bildschirm um 1,
-					//nachdem die Server-Anfrage ausgeführt wurde.
+					//nachdem die Server-Anfrage ausgefÃ¼hrt wurde.
 					stock.innerHTML = parseInt(stock.innerHTML)-1;
 				}
 			};
 			
-			//Führe Anfrage aus.
+			//FÃ¼hre Anfrage aus.
 			request.open('POST', ajaxurl, true);
 			request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 			request.send(data);
