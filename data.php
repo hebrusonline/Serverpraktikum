7311a0bf6eeef1417eed53e66bc46af33fa389d4
ThomFri <36042923+ThomFri@users.noreply.github.com>
2020-10-24 15:42:55 +0200

    Update data.php
    produktdatenbank -> produkttabelle

--- data.php
+++ data.php
@@ -1,16 +1,16 @@
 <?php
 //Hier werden Datenbankinhalte verwaltet.
 
-//Überprüfe den Grund für den Aufruf:
+//ÃœberprÃ¼fe den Grund fÃ¼r den Aufruf:
 if($_POST['id'] && $_POST['name']&& $_POST['type']=='kundentabelle'){
 	//Aufruf, um Zeile in Kundentabelle zu schreiben.
 	insert_data_kunden('kundentabelle', $_POST);
 	header('location: index.php');
 }else if($_POST['pid'] && $_POST['stock']){
 	//Aufruf, um Zeile in Produkttabelle zu schreiben.
-	insert_data_produkte('produktdatenbank', $_POST);
+	insert_data_produkte('produkttabelle', $_POST);
 }else{
-	//Grund für Aufruf nicht ersichtlich.
+	//Grund fÃ¼r Aufruf nicht ersichtlich.
 	//Schreibt Zeile in die Datei '/var/log/apache2/error.log' auf Ihrem Server.
 	error_log("Es ist ein Fehler aufgetreten:".$_POST['data']);	
 }
@@ -27,7 +27,7 @@ function get_data($table_name){
 	try{
 	//Datenbankverbindung hestellen.
 	$conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", "$user", "$pwd");
-	//Sorgt für lesbare Fehlermeldung.
+	//Sorgt fÃ¼r lesbare Fehlermeldung.
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	 
 	//SQL Befehl vorbereiten.
@@ -40,7 +40,7 @@ function get_data($table_name){
 	$selectStatement->execute();
 	//$selectStatement->execute([$table_name]);	
 	
-    //Wenn das Ergebnis Zeilen enthält, solle die Ergebisse zurückgegeen werden.
+    //Wenn das Ergebnis Zeilen enthÃ¤lt, solle die Ergebisse zurÃ¼ckgegeen werden.
 	if($selectStatement->rowCount()){
 		//Speichere alle Zeilen in result.
 		$result = $selectStatement->fetchAll();
@@ -51,7 +51,7 @@ function get_data($table_name){
 	//Datenbankverbindung trennen.
 	$conn = null;
 	
-	//Ergebnis zurückgeben.
+	//Ergebnis zurÃ¼ckgeben.
 	return $result;
 	} catch(PDOException $e){
 		echo "Connection failed: " . $e->getMessage();
@@ -69,7 +69,7 @@ function insert_data_kunden($table, $data){
 	//Datenbankverbindung hestellen.
 	$conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", "$user", "$pwd");
 	 
-	//SQL Befehl ausführen.
+	//SQL Befehl ausfÃ¼hren.
 	$insert_query = "INSERT INTO ".$table."(id,name) VALUES(".$data['id'].",'".$data['name']."')";
 
 	$conn->query($insert_query);
@@ -93,7 +93,7 @@ function insert_data_produkte($table, $data){
 	//Datenbankverbindung hestellen.
 	$conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", "$user", "$pwd");
 	 
-	//SQL Befehl ausführen.
+	//SQL Befehl ausfÃ¼hren.
 	$insert_query = "UPDATE ".$table." SET lager =".$data[stock]." WHERE ".$table.".id = ".$data[pid];
 
 	$conn->query($insert_query);
